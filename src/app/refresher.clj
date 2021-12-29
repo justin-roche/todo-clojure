ed58515449f224814543555effa6b2010b19d33a
Justin Roche <justinroche@Justins-Mini.lan>
2021-12-29 14:15:39 -0500

    move dev files to dev-resources

--- src/app/refresher.clj
+++
@@ -1,8 +0,0 @@
-(ns app.refresher
-  (:require
-   [mount.core :as mount :refer [defstate]]
-   [clojure.tools.namespace.repl :as rp :refer [set-refresh-dirs refresh]]))
-
-(set-refresh-dirs "src/app" "test/app")
-
-(refresh :after 'app.user/refresh-and-run-tests)
